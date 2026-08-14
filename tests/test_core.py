2ce32b0fb530db9e9a257e83c09f1f907d86101c
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-08-14 08:45:15 -0600

    Fix tests
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tests/test_core.py
+++ tests/test_core.py
@@ -603,7 +603,7 @@ def test_standard_tap_tests(  # noqa: PLR0913
 
     responses.add(
         responses.GET,
-        "https://cloud.getdbt.com/api/v2/accounts/1000/connections",
+        "https://cloud.getdbt.com/api/v3/accounts/1000/connections",
         json=connections_response,
         status=200,
     )
@@ -698,7 +698,6 @@ def test_standard_tap_tests(  # noqa: PLR0913
         ),
     ],
 )
-
 def test_dynamic_base_url(
     base_url_config: str, stream_cls: type, base_url_expected: str
 ) -> None:
